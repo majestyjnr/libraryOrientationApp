5224de500fd97c4ff5f5827c9c2402be8153a851
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-06 04:16:57 -0700

    Commit Updates

---
+++ lib/screens/student/auth/Signin.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Signin extends StatefulWidget {
+  Signin({Key key}) : super(key: key);
+
+  @override
+  _SigninState createState() => _SigninState();
+}
+
+class _SigninState extends State<Signin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
