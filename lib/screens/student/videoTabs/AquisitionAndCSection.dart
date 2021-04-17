869cb0723b191080eb378fb0a5551fc5ed13baa4
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-04-17 02:47:19 -0700

    Commit Updates

--- lib/screens/student/videoTabs/AquisitionAndCSection.dart
+++ lib/screens/student/videoTabs/AquisitionAndCSection.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class AcquisitionSection extends StatefulWidget {
   AcquisitionSection({Key key}) : super(key: key);
@@ -8,10 +12,54 @@ class AcquisitionSection extends StatefulWidget {
 }
 
 class _AcquisitionSectionState extends State<AcquisitionSection> {
+  FlickManager flickManager;
+  var _videoURL;
+  @override
+  void initState() {
+    super.initState();
+    FirebaseFirestore.instance
+        .collection('Videos Info')
+        .where('videoSection', isEqualTo: 'Acquisition Section')
+        .get()
+        .then((snapshot) {
+      snapshot.docs.forEach((doc) {
+        setState(() {
+          _videoURL = doc['videoURL'];
+        });
+      });
+    });
+
+    flickManager = FlickManager(
+      videoPlayerController: VideoPlayerController.network(_videoURL),
+    );
+  }
+
+  @override
+  void dispose() {
+    flickManager.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-    
+    return SingleChildScrollView(
+      physics: BouncingScrollPhysics(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          (_videoURL != null)
+              ? FlickVideoPlayer(
+                  flickManager: flickManager,
+                )
+              : Text(
+                  'Loading...',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.blue,
+                  ),
+                ),
+        ],
+      ),
     );
   }
 }
