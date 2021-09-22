4ee48ff45cabada14109aad9ee4267000f6a79b9
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-09-22 09:44:28 -0700

    Commit Updates

--- lib/screens/student/faqs/AskQuestion.dart
+++ lib/screens/student/faqs/AskQuestion.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:papercups_flutter/papercups_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AskQuestion extends StatefulWidget {
